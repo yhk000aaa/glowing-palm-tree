37c0ce6cd2643124a98d84f8f7772ec197a9126b
xipengfei <xipengfei@zjcyou.com>
2022-08-24 18:00:44 +0800

    主界面退出按钮功能添加

--- Assets/Scripts/GameLogic/UI/StartUI.cs
+++ Assets/Scripts/GameLogic/UI/StartUI.cs
@@ -7,7 +7,7 @@ using XHFrameWork;
 public class StartUI : BaseUI
 {
     private Button confirmBtn;
-    
+    private Button leaveBtn;
     public override EnumUIType GetUIType()
     {
         return EnumUIType.StartUI;
@@ -18,16 +18,29 @@ public class StartUI : BaseUI
         base.OnStart();
         this.confirmBtn = this.gameObject.transform.Find("startButton").GetComponent<Button>();
         this.confirmBtn.onClick.AddListener(this.clickConfirmBtnEvent);
+        this.leaveBtn = this.gameObject.transform.Find("leaveButton").GetComponent<Button>();
+        this.leaveBtn.onClick.AddListener(this.clickLeaveBtnEvent);
     }
 
     protected override void destroyAction()
     {
         this.confirmBtn.onClick.RemoveListener(this.clickConfirmBtnEvent);
+        this.leaveBtn.onClick.RemoveListener(this.clickLeaveBtnEvent);
         base.destroyAction();
     }
 
     void clickConfirmBtnEvent()
     {
         EducationNode.Instance.tryEnterBattle();
+    }
+
+    void clickLeaveBtnEvent()
+    {
+        if (UnityEditor.EditorApplication.isPlaying){
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else{
+            Application.Quit();
+        }
     }
 }
